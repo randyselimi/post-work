816ca2da5ce76caff43c268623c2e6350b6451d9
Randy Selimi <selimi.4@osu.edu>
2024-01-31 12:41:49 -0500

    fix: import user modal

--- src/lib/network-dashboard.svelte
+++ src/lib/network-dashboard.svelte
@@ -1,24 +1,28 @@
-<!-- <div class="dashboard">
-    <div class="dashboard-title">
-        <h1>Network</h1>
-    </div>
-    <div class="tile">
-        <div class="tile-header">
-            <h3>Coworkers</h3>
-        </div>
+<script>
+	import UserModal from './modals/user-modal.svelte';
+</script>
 
-        <div class="tile-row">
-            <UserModal user={'User A'} />
-            <UserModal user={'User B'} />
-            <UserModal user={'User C'} />
-        </div>
-        <div class="tile-header">
-            <h3>Organization</h3>
-        </div>
-        <div class="tile-row">
-            <UserModal user={'User A'} />
-            <UserModal user={'User B'} />
-            <UserModal user={'User C'} />
-        </div>
-    </div>
-</div> -->
+<div class="dashboard">
+	<div class="dashboard-title">
+		<h1>Network</h1>
+	</div>
+	<div class="tile">
+		<div class="tile-header">
+			<h3>Coworkers</h3>
+		</div>
+
+		<div class="tile-row">
+			<UserModal user={'User A'} />
+			<UserModal user={'User B'} />
+			<UserModal user={'User C'} />
+		</div>
+		<div class="tile-header">
+			<h3>Organization</h3>
+		</div>
+		<div class="tile-row">
+			<UserModal user={'User A'} />
+			<UserModal user={'User B'} />
+			<UserModal user={'User C'} />
+		</div>
+	</div>
+</div>
