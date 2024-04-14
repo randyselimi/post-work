cfd48bfa2b68629be61230e850b252bcaa4d9e6b
Randy Selimi <email@example.com>
2024-04-14 02:20:29 -0400

    feat(list): started on table components

---
+++ src/lib/tables/user-table.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let columns = [
+		{ label: 'Name', key: 'name' },
+		{ label: 'Role', key: 'role' },
+		{ label: 'Email', key: 'email' },
+		{ label: 'Team', key: 'team' }
+	];
+</script>
---
+++ src/lib/tables/work-table.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let columns = [
+		{ label: 'Title', key: 'title' },
+		{ label: 'Status', key: 'status' },
+		{ label: 'Hours', key: 'hours' },
+		{ label: 'Assigned To', key: 'assignedTo' },
+		{ label: 'Due Date', key: 'dueDate' }
+	];
+</script>
